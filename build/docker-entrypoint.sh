b1fd83b6fff01c4a84e8d0d906745346a60da663
Martin <skydiver@users.noreply.github.com>
2016-09-20 17:49:24 -0300

    create laravel app

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# CREATE LARAVEL APP IF NEEDED
+if ! [ "$(ls -A /var/www/html/)" ]; then
+    cd /tmp/
+    composer create-project --prefer-dist laravel/laravel app
+    mv /tmp/app/* /var/www/html/
+    mv /tmp/app/.[!.]* /var/www/html/
+fi
+
 # APACHE CUSTOM USER
 chown -R martin. /var/www/html
 
