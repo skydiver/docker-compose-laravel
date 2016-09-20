abaf8b051ab725566b450af19b568fb954f923b4
Martin <skydiver@users.noreply.github.com>
2016-09-20 20:46:23 -0300

    change laravel db config on build

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -2,10 +2,18 @@
 
 # CREATE LARAVEL APP IF NEEDED
 if ! [ "$(ls -A /var/www/html/)" ]; then
+
     cd /tmp/
     composer create-project --prefer-dist laravel/laravel app
     mv /tmp/app/* /var/www/html/
     mv /tmp/app/.[!.]* /var/www/html/
+
+    # UPDATE LARAVEL CONFIG
+    sed -i -e 's/DB_HOST=127.0.0.1/DB_HOST=db/g' /var/www/html/.env
+    sed -i -e 's/DB_DATABASE=homestead/DB_DATABASE=laravel/g' /var/www/html/.env
+    sed -i -e 's/DB_USERNAME=homestead/DB_USERNAME=root/g' /var/www/html/.env
+    sed -i -e 's/DB_PASSWORD=secret/DB_PASSWORD=root/g' /var/www/html/.env
+
 fi
 
 # APACHE CUSTOM USER
